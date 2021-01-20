435a6b167b870d16dcf6d9741e212fbccadf224a
Peter Ting <pting1995@gmail.com>
2021-01-19 17:37:43 -0800

    added questions

--- index.js
+++ index.js
@@ -1,13 +1,38 @@
-// TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const util = require('util');
+
+// const writeFileAsync = util.promisify(fs.writeFile);
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [ 
+"What are the contribution guidelines?",
+"What are the test instructions?",
+"What licenses?",
+"What is your GitHub username?",
+"What is your email address"];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const questionPrompts = () =>
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "What is your readme title?",
+            name: "title"
+        },
+        {
+            type: "input",
+            message: "What is your description?",
+            name: "desc"
+        },
+        {
+            type: "input",
+            message: "What are the installation instructions?",
+            name: "install"
+        },
+        {
+            type: "input",
+            message: "What is the usage information?",
+            name: "use"
+        }
 
-// TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
-init();
+    ])
